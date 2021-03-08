4796cff72f7edb00b47e9d7ebc59bab07d59f352
Vyacheslav Bezborodov <v.bezborodov@cian.ru>
2021-03-08 13:34:07 +0300

    add mobile page

---
+++ assets/mobile_files/vendor-react-dom.69059437.chunk.min.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp__wix_thunderbolt_app=window.webpackJsonp__wix_thunderbolt_app||[]).push([[118],{159:function(n,t,e){"use strict";var o=e(160);Object.assign(window.ReactDOM,o),window.externalsRegistry.reactDOM.onload()},3:function(n,t){n.exports=React}},[[159,2,154]]]);
+//# sourceMappingURL=https://static.parastorage.com/services/wix-thunderbolt/dist/vendor-react-dom.69059437.chunk.min.js.map
